efc8380c4e03f850e9b117c0600978b15b5bfc72
Serguei Ossokine <serguei.ossokine@aei.mpg.de>
2023-02-12 18:24:29 +0100

    Make sure t=0 is peak of frame invariant amplitude

--- pyseobnr/eob/utils/waveform_ops.py
+++ pyseobnr/eob/utils/waveform_ops.py
@@ -5,20 +5,26 @@ Additional utility functions.
 from typing import Any,Dict
 import numpy as np
 
-def frame_inv_amp(modes:Dict[Any,Any],ell_max:int=2)->np.ndarray:
-    """Compute the frame-invariant amplitude
+def frame_inv_amp(modes:Dict[Any,Any],ell_max:int=2,use_symm=True)->np.ndarray:
+    """Compute the frame-invariant amplitude.
+    By default, assumes that we have aligned-spin symmetry.
 
     Args:
         modes (Dict[Any,Any]): The dictionary of modes
         ell_max (int): The maximum l to use
+        use_symm (bool): Assume up-down aligned symmetry
 
     Returns:
         np.ndarray: The frame-invariant ampltiude time series
     """
     total = 0.0
+
     for mode in modes.keys():
         ell,m = mode
         if ell>ell_max:
             continue
-        total+=np.abs(modes[mode])**2
+        if use_symm:
+            total+=2*np.abs(modes[mode])**2
+        else:
+            total+=np.abs(modes[mode])**2
     return np.sqrt(total)
