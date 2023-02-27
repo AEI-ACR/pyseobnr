afef8a32a0d42e062a1e4046d340ba8ee1d76dab
lpompili <lorenzo.pompili@aei.mpg.de>
2023-02-27 12:49:46 +0100

    Add missing models in init.py

--- pyseobnr/auxiliary/external_models/__init__.py
+++ pyseobnr/auxiliary/external_models/__init__.py
@@ -1,1 +1,1 @@
-from .external_models import NRHybSur2dq15Model,NRHybSur3dq8Model,SEOBNRv4HM_LAL,Model,NRModel_SXS
+from .external_models import NRHybSur2dq15Model,NRHybSur3dq8Model,SEOBNRv4HM_LAL,Model,NRModel_SXS,NRModel_LVC,NRModel_RIT
