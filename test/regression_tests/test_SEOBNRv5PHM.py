e0346c82cd89a2132b45d4fe122a875f3b997fc1
Serguei Ossokine <serguei.ossokine@aei.mpg.de>
2023-03-02 15:19:26 +0100

    Fix pipeline

--- test/regression_tests/test_SEOBNRv5PHM.py
+++ test/regression_tests/test_SEOBNRv5PHM.py
@@ -3,7 +3,7 @@ Test that the precessing SEOBNRv5PHM model has not changed.
 Follows the procedure of IMRPhenomXPHM/IMRPhenomTPHM tests
 in LALSuite.
 
-This file was automatically generated on 2023-03-01 21:20:58.294959.
+This file was automatically generated on 2023-03-02 15:18:44.993926.
 It should be regenrated using regenerate_SEOBNRv5PHM.py whenever
 understood changes to the model are made.
 """
@@ -45,6 +45,7 @@ def gen_test_data(test_type):
     s2z = -0.1
     f_max = 1024.0
     f_min = 0.0157 / (Mt * np.pi * lal.MTSUN_SI)
+    deltaF = 0.125
     params_dict = {
         "mass1": m1,
         "mass2": m2,
@@ -55,6 +56,7 @@ def gen_test_data(test_type):
         "spin2y": s2y,
         "spin2z": s2z,
         "deltaT": dt,
+        "deltaF": deltaF,
         "f22_start": f_min,
         "phi_ref": phiRef,
         "distance": distance,
