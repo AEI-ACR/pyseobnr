1dc6ff60ecf361ba080497aa1e93e4890651d110
Raffi Enficiaud <raffi.enficiaud@aei.mpg.de>
2024-03-13 15:40:55 +0100

    Avoid unnecessary imports and computations at load time
    * remove "import everything" from code (on-going effort)
    * evaluating the qnms lazily
    * faster dynamics interpolation + tests
    
    Other:
    * isort/cython-lint/flake8 configuration in pyproject
    * upgrading pre-commits to use isort, flake8 and cython-lint checks
    * CI and tox improvements

--- pyseobnr/eob/dynamics/integrate_ode.py
+++ pyseobnr/eob/dynamics/integrate_ode.py
@@ -4,16 +4,14 @@ Contains functions associated with evolving the equations of motion
 """
 
 import numpy as np
-from scipy.interpolate import CubicSpline
-from scipy.signal import argrelmin
-from numba import jit
-
 import pygsl_lite.errno as errno
 import pygsl_lite.odeiv2 as odeiv2
+from numba import jit
+from scipy.interpolate import CubicSpline
 
+from ..utils.utils import interpolate_dynamics, iterative_refinement
 from .initial_conditions_aligned_opt import computeIC_opt
-from .rhs_aligned import get_rhs, augment_dynamics
-
+from .rhs_aligned import augment_dynamics, get_rhs
 
 step = odeiv2.pygsl_lite_odeiv2_step
 _control = odeiv2.pygsl_lite_odeiv2_control
@@ -120,7 +118,7 @@ def compute_dynamics_opt(
     omega_previous = omega0
     res_gsl = []
     ts = []
-    omegas = []
+
     ts.append(0.0)
     res_gsl.append(y)
 
@@ -200,13 +198,13 @@ def compute_dynamics_opt(
         step_back = ts[-1] - ts[idx_close]
     dyn_coarse = np.c_[ts[:idx_close], dyn[:idx_close]]
     dyn_fine = np.c_[ts[idx_close:], dyn[idx_close:]]
-    """
-    print(f"t_desired={t_desired}")
-    print(f"len(dyn_coarse)={len(dyn_coarse)}")
-    print(f"len(dyn_fine)={len(dyn_fine)}")
-    print(f"End time: {ts[-1]}")
-    print(f"idx_close:{idx_close}, t[idx_close]={ts[idx_close]}")
-    """
+
+    # print(f"t_desired={t_desired}")
+    # print(f"len(dyn_coarse)={len(dyn_coarse)}")
+    # print(f"len(dyn_fine)={len(dyn_fine)}")
+    # print(f"End time: {ts[-1]}")
+    # print(f"idx_close:{idx_close}, t[idx_close]={ts[idx_close]}")
+
     dyn_coarse = augment_dynamics(dyn_coarse, chi_1, chi_2, m_1, m_2, H)
     dyn_fine = augment_dynamics(dyn_fine, chi_1, chi_2, m_1, m_2, H)
     t_peak = None
@@ -228,75 +226,3 @@ def compute_dynamics_opt(
     dyn_fine = augment_dynamics(dyn_fine, chi_1, chi_2, m_1, m_2, H)
 
     return dyn_coarse, dyn_fine
-
-
-def iterative_refinement(f, interval, levels=2, dt_initial=0.1, pr=False):
-    """
-    Attempts to find the peak of Omega/pr iteratively.
-    Needed to ensure accurate attachment when the attachment point is the last point of the dynamics.
-
-    Args:
-        f (PPoly): derivative of the splined dynamics
-        interval (list): interval where to look for the peak
-        levels (int): number of iterations
-        dt_initial (float): initial time step
-        pr (bool): whether the end of the dynamics is due to a peak of pr instead of Omega
-
-    Returns:
-        np.array: interpolated dynamics array
-
-    """
-    left = interval[0]
-    right = interval[1]
-    for n in range(1, levels + 1):
-        dt = dt_initial / (10**n)
-        t_fine = np.arange(interval[0], interval[1], dt)
-        deriv = np.abs(f(t_fine))
-
-        mins = argrelmin(deriv, order=3)[0]
-        if len(mins) > 0:
-            result = t_fine[mins[0]]
-
-            interval = max(result - 10 * dt, left), min(result + 10 * dt, right)
-
-        else:
-            if pr:
-                return interval[-1]
-            else:
-                return (interval[0] + interval[-1]) / 2
-    return result
-
-
-def interpolate_dynamics(dyn_fine, dt=0.1, peak_omega=None, step_back=250.0):
-    """
-    Interpolate the dynamics to a finer grid.
-    This replaces stepping back that was used in older EOB models.
-
-    Args:
-        dyn_fine (np.array): dynamics array
-        dt (float): time step to which to interpolate
-        peak_omega (float): position of the peak (stopping condition for the dynamics)
-        step_back (float): step back relative to the end of the dynamics
-
-    Returns:
-        np.array: interpolated dynamics array
-
-    """
-
-    res = []
-    n = len(dyn_fine)
-
-    if peak_omega:
-        start = max(peak_omega - step_back, dyn_fine[0, 0])
-        t_new = np.arange(start, peak_omega, dt)
-
-    else:
-        t_new = np.arange(dyn_fine[0, 0], dyn_fine[-1, 0], dt)
-
-    for i in range(1, dyn_fine.shape[1]):
-        intrp = CubicSpline(dyn_fine[:, 0], dyn_fine[:, i])
-        res.append(intrp(t_new))
-
-    res = np.array(res)
-    res = res.T
-    return np.c_[t_new, res]
---
+++ pyseobnr/eob/utils/utils.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+from scipy.signal import argrelmin
+
+
+def iterative_refinement(f, interval, levels=2, dt_initial=0.1, pr=False):
+    """
+    Attempts to find the peak of Omega/pr iteratively.
+    Needed to ensure accurate attachment when the attachment point is the last point of the dynamics.
+
+    Args:
+        f (PPoly): derivative of the splined dynamics
+        interval (list): interval where to look for the peak
+        levels (int): number of iterations (should be greater than 2)
+        dt_initial (float): initial time step
+        pr (bool): whether the end of the dynamics is due to a peak of pr instead of Omega
+
+    Returns:
+        np.array: interpolated dynamics array
+
+    """
+
+    assert levels > 1
+
+    left = interval[0]
+    right = interval[1]
+    result = None
+
+    for n in range(1, levels + 1):
+        dt = dt_initial / (10**n)
+        t_fine = np.arange(interval[0], interval[1], dt)
+        deriv = np.abs(f(t_fine))
+
+        mins = argrelmin(deriv, order=3)[0]
+        if len(mins) > 0:
+            result = t_fine[mins[0]]
+
+            interval = max(result - 10 * dt, left), min(result + 10 * dt, right)
+
+        else:
+            if pr:
+                return interval[-1]
+            else:
+                return (interval[0] + interval[-1]) / 2
+
+    assert result is not None
+    return result
+
+
+def interpolate_dynamics(dyn_fine, dt=0.1, peak_omega=None, step_back=250.0):
+    """
+    Interpolate the dynamics to a finer grid.
+    This replaces stepping back that was used in older EOB models.
+
+    Args:
+        dyn_fine (np.array): dynamics array
+        dt (float): time step to which to interpolate
+        peak_omega (float): position of the peak (stopping condition for the dynamics)
+        step_back (float): step back relative to the end of the dynamics
+
+    Returns:
+        np.array: interpolated dynamics array
+
+    """
+
+    if peak_omega:
+        start = max(peak_omega - step_back, dyn_fine[0, 0])
+        t_new = np.arange(start, peak_omega, dt)
+
+    else:
+        t_new = np.arange(dyn_fine[0, 0], dyn_fine[-1, 0], dt)
+
+    intrp = CubicSpline(dyn_fine[:, 0], dyn_fine[:, 1:])
+    res = intrp(t_new)
+
+    return np.c_[t_new, res]
